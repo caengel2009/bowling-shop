96ad319264e0e299611a0ab01a623213c9ce0f45
caengel2009 <caengel2009@users.noreply.github.com>
2015-07-10 18:12:33 -0500

    Adding initial Angular module, loaded via bower

---
+++ public/javascripts/app.js
@@ -0,0 +1,7 @@
+// Application Definition
+var bowlingShopApp = angular.module('BowlingShop', []);
+
+bowlingShopApp.controller('PhoneListCtrl', function ($scope) {
+
+  $scope.greeting = "Hi";
+});
